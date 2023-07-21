1cab33589a18182efa71cc5e045390ee28d4eb81
Jonathan Velez <jonathan.v.giraldo@gmail.com>
2023-07-21 11:11:59 -0500

    Initial commit

---
+++ src/main/java/com/epico/efficent/EfficentApplication.java
@@ -0,0 +1,13 @@
+package com.epico.efficent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfficentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfficentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epico/efficent/EfficentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epico.efficent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
